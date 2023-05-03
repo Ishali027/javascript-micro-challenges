632b0f47a529d823db855277f8cdb19d3c58f69f
Ismail Ali <ishaliprime@gmail.com>
2023-05-03 00:32:52 -0500

    wrote a function that adds the first and last values in an array using the dot length operator

--- script.js
+++ script.js
@@ -22,5 +22,14 @@ console.log(result);
 //done 110-conditional
 
 
+// 120-Arrays
+let numbers = [2, 4, 6, 8, 10];
+function addFirstAndLast( array){
+    let sum = 0;
+   sum += array[0] + array[array.length-1];
+    return sum
 
+}
+result = addFirstAndLast(numbers);
+console.log(result);
 // done 120-arrays
